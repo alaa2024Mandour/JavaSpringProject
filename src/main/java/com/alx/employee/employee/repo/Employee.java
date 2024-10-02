709be1a10837038798e45453d9b64f40cb7c2ae1
alaa2024Mandour <alaaMandour20242@gmail.com>
2024-10-02 17:24:22 +0300

    annotations

--- src/main/java/com/alx/employee/employee/repo/Employee.java
+++ src/main/java/com/alx/employee/employee/repo/Employee.java
@@ -1,65 +1,20 @@
 package com.alx.employee.employee.repo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     Long id;
     String name;
     String address;
     String salary;
     String hiring_date;
-
-    public Employee() {}
-
-    public Employee(Long id, String name, String address, String salary, String hiring_date) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-        this.hiring_date = hiring_date;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getSalary() {
-        return salary;
-    }
-
-    public void setSalary(String salary) {
-        this.salary = salary;
-    }
-
-    public String getDate() {
-        return hiring_date;
-    }
-
-
-    public void setDate(String hiring_date) {
-        this.hiring_date = hiring_date;
-    }
-
-
-
 }
